664621ecc0ea7b80dd0fb08f67d6b9e3776d4940
VA <moi@varoper.com>
2025-08-19 17:48:00 -0600

    Add a few more items to state management

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -6,16 +6,16 @@ const useStore = create((set) => ({
     topView: true,
     activeId: null,
     overId: null,
-    //   drummer: null,
-    //   stern: null,
+    drummer: null,
+    stern: null,
 
     // Actions
     setSeatingChart: (chart) => set({ seatingChart: chart }),
     toggleTopView: () => set((state) => ({ topView: !state.topView })),
     setActiveId: (id) => set({ activeId: id }),
     setOverId: (id) => set({ overId: id }),
-    //   setDrummer: (drummer) => set({ drummer }),
-    //   setStern: (stern) => set({ stern }),
+    setDrummer: (drummer) => set({ drummer }),
+    setStern: (stern) => set({ stern }),
 }));
 
 export default useStore;
